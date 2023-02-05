f1d93a48833e57b67b60c4e41a75528de2983f56
FrancoisVincent <francoisvincent@laposte.net>
2023-02-05 19:24:01 +0100

    page d'acceuil

--- ECF/acceuil.php
+++ ECF/acceuil.php
@@ -15,7 +15,13 @@ require "head.php";
 
 
     <main>
-
+        <div class="ventalis">
+            <p>
+                Ventalis, est une entreprise spécialisée dans la vente de produit marketing ainsi que dans la visibilité de leurs clients.Ventalis fait un chiffre d’affaires de 3 millions d’euros, c’est une Licorne100% française composée de 50 salariés répartis dans toute la France.
+                <br>
+                Ventalis est dans une démarche écologique : nous reversons 20% de notre chiffre d’affaires annuel dans les projets qui œuvre pour cela
+            </p>
+        </div>
     </main>
 
     <footer>
