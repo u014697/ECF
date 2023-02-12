8ecea9e791596776e978680b14139dc0f515819b
FrancoisVincent <francoisvincent@laposte.net>
2023-02-12 18:58:01 +0100

    petites corrections

--- ECF/ref/connect copy.php
+++
@@ -1,3 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=https://ecf-fv.go.yj.fr;dbname=mwryvfpk_ventalis', 'admin@ecf-fv.go.yj.fr', '212-Renaudin');
-?>
--- ECF/ref/connectprod.php
+++
@@ -1,3 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=mwryvfpk_ventalis;charset=utf8mb4', 'mwryvfpk_superuser', '212-Renaudin');
-?>
